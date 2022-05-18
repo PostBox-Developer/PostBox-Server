b7c715c9ae22371d3b691d6d145a57f13422646b
BambooKim <rlaqjarn1008@gmail.com>
2022-05-18 14:40:15 +0900

    Implement Post Create API

--- post/views.py
+++ post/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+from .serializer import PostSerializer
+from rest_framework import generics
+
+from user.models import User
+
+class PostAPI(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        author_id = self.request.data.get("author_id")
+
+        userInstance = User.objects.get(user_id=author_id)
+
+        serializer.save(author=userInstance)
