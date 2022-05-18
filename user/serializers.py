2281b09223cc992e49ae02f4b13b16c40db584e4
sschan99 <seungchan1219@gmail.com>
2022-05-18 16:01:01 +0900

    add: user urls, user create view

--- user/serializers.py
+++ user/serializers.py
@@ -5,3 +5,11 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['user_id', 'username', 'root_folder', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            user_id = validated_data['user_id'],
+            username = validated_data['username'],
+            password = validated_data['password']
+        )
+        return user
--- user/views.py
+++ user/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics
 from user.models import User
 from user.serializers import UserSerializer
 
-class UserCreate(generics.CreateAPIView):
+class UserCreate(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
