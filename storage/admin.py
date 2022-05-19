594a54de03080c0e3a985a98496ebb3459195226
jm choi <527wlals@gmail.com>
2022-05-19 16:09:15 +0900

    add: connect storage models and admin page

--- storage/admin.py
+++ storage/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import File, Folder
 
 # Register your models here.
+@admin.register(Folder)
+class FolderAdmin(admin.ModelAdmin):
+    list_display = ['foldername', 'creater']
+    list_display_links = ['foldername', 'creater']
+
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    list_display = ['filename', 'uploader']
+    list_display_links = ['filename', 'uploader']
