4db7ff20d7ca70bcf63af89bad0f741e0bd244ff
BambooKim <rlaqjarn1008@gmail.com>
2022-05-24 01:28:46 +0900

    add: get_home_folder/, get_folder_contents/, upload_file/, modify_filename/

--- storage/urls.py
+++ storage/urls.py
@@ -4,10 +4,17 @@ from . import views
 app_name = "storage"
 
 urlpatterns = [
+    path("get_home_folder/", views.get_home_folder),
+    path("get_folder_contents/", views.get_folder_contents),
+    path("upload_file/", views.upload_file),
+    path("modify_filename/", views.modify_filename),
+
     path("get_file_list/", views.FileListCreateAPIView.as_view()),
     path("get_folder_list/", views.FolderListCreateAPIView.as_view()),
-    path("upload_file/", views.FileListCreateAPIView.as_view()),
+    #path("upload_file/", views.FileListCreateAPIView.as_view()),
     path("create_new_folder/", views.FolderListCreateAPIView.as_view()),
+
+
     path("delete_file/", views.delete_file),
     path("delete_folder/", views.delete_folder),
 ]
--- storage/views.py
+++ storage/views.py
@@ -1,3 +1,4 @@
+from pydoc import cli
 from .models import File, Folder
 from .serializers import *
 from rest_framework import generics
@@ -6,6 +7,8 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from django.http import JsonResponse
+from django.utils import timezone
 
 import boto3
 from .boto3_config import secrets
@@ -21,6 +24,153 @@ resource = boto3.resource('s3',
                         region_name = secrets.get("AWS_DEFAULT_REGION"))
 
 BucketName = secrets.get("BUCKET_NAME")
+
+'''
+홈 디렉토리 폴더-파일 목록 조회
+GET: storage/get_home_folder/
+'''
+@api_view(['GET'])
+@authentication_classes((TokenAuthentication, ))
+def get_home_folder(request):
+    login_user = request.user
+
+    # 루트 폴더 조회 
+    rootFolder = Folder.objects.filter(
+        creater = login_user,           # creater가 로그인된 유저이고
+        parent_folder__isnull=True      # parent_folder가 null인 것
+    )[0]
+
+    # parent_folder가 rootFolder인 폴더와 파일들 조회
+    folders = Folder.objects.filter(
+        creater = login_user,
+        parent_folder = rootFolder
+    )
+
+    files = File.objects.filter(
+        uploader = login_user,
+        parent_folder = rootFolder
+    )
+
+    return JsonResponse({
+        "message": "Success",
+        "current_folder_id": rootFolder.id,
+        "current_folder_name": "root/",
+        "folder_results": list(folders.values()), #FolderListSerializer(folders, many=True),
+        "file_results": list(files.values()), #FileListSerializer(files, many=True)
+    }, json_dumps_params = {'ensure_ascii': True})
+
+'''
+폴더-파일 목록 조회: 어느 폴더에 들어갔을 때, 그 폴더에 존재하는 하위 폴더와 파일들의 목록 조회
+GET: storage/get_folder_contents/
+'''
+@api_view(['GET'])
+@authentication_classes((TokenAuthentication, ))
+def get_folder_contents(request):
+    login_user = request.user
+
+    data = request.data
+    folder_id = data.get("folder_id")       # 폴더 클릭해서 들어갔을 때, 클릭한 폴더의 PK
+    folder_path = data.get("folder_path")
+
+    # PK가 folder_id인 폴더 조회 
+    clickedFolder = Folder.objects.get(
+        pk = folder_id                  # PK가 folder_id
+    )
+
+    # parent_folder가 parentFolder인 폴더와 파일들 조회
+    folders = Folder.objects.filter(
+        creater = login_user,
+        parent_folder = clickedFolder
+    )
+
+    files = File.objects.filter(
+        uploader = login_user,
+        parent_folder = clickedFolder
+    )
+
+    return JsonResponse({
+        "message": "Success",
+        "current_folder_id": folder_id,
+        "current_folder_name": clickedFolder.foldername,
+        "parent_folder_id": clickedFolder.parent_folder.id,
+        "folder_results": list(folders.values()), 
+        "file_results": list(files.values()), 
+    }, json_dumps_params = {'ensure_ascii': True})
+
+'''
+업로드된 파일 메타데이터 전송: S3 업로드에 성공한 파일의 파일명 저장-PK 응답
+POST: storage/upload_file/
+'''
+@api_view(['POST'])
+@authentication_classes((TokenAuthentication, ))
+def upload_file(request):
+    login_user = request.user
+
+    data = request.data
+    filename = data.get("filename")
+    parent_folder_id = data.get("parent_folder_id")
+    #path = data.get("path")
+
+    uploadedFile = File(
+        filename = filename,
+        uploader = login_user,
+        parent_folder = Folder.objects.filter(
+            creater = login_user,
+            id = parent_folder_id
+        )[0]
+    )
+    uploadedFile.save()
+
+    return Response(FileSerializer(uploadedFile).data)
+
+'''
+파일명 변경: 업로드된 파일의 파일명 변경
+POST: storage/modify_filename/
+'''
+@api_view(['POST'])
+@authentication_classes((TokenAuthentication, ))
+def modify_filename(request):
+    login_user = request.user
+
+    data = request.data
+    file_id = data.get("file_id")
+    src_name = data.get("src_name")
+    dst_name = data.get("dst_name")
+    parent_folder_id = data.get("parent_folder_id")
+    _path = data.get("path")
+
+    ## DB의 파일명 변경, modified_at 현재시간 삽입
+    target = File.objects.filter(
+        id = file_id,
+        uploader = login_user
+    )[0]
+    target.filename = dst_name
+    target.modified_at = timezone.now()
+    target.save()
+
+    ## S3의 객체 복사->기존 객체 삭제
+    s3_key_path = str(login_user.user_id + "/" + _path)
+
+    src_s3_object = {
+        "Bucket": BucketName,
+        "Key": s3_key_path + src_name
+    }
+    resource.meta.client.copy(src_s3_object, BucketName, s3_key_path + dst_name)
+
+    client.delete_object(
+        Bucket = BucketName,
+        Key = s3_key_path + src_name
+    )
+
+    return JsonResponse({
+        "message": "Success",
+        "id": target.id,
+        "filename": target.filename,
+        "parent_folder_id": target.parent_folder.id,
+        "path": _path,
+        "created_at": target.created_at,
+        "modified_at": target.modified_at
+    }, json_dumps_params = {'ensure_ascii': True})
 
 class FileListCreateAPIView(generics.ListCreateAPIView):
     
